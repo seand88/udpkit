8fec94912ed1fb7bfab34c49425e7fb5282e9fe7
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-02-07 11:17:50 +0100

    re-added android project for buildint native shared lib

---
+++ src/native/android/jni/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)  
+include $(CLEAR_VARS)  
+LOCAL_LDLIBS := -llog  
+LOCAL_MODULE    := udpkit_android
+LOCAL_SRC_FILES := ../../shared/common.cpp ../../shared/socket.cpp ../../shared/timers.cpp
+include $(BUILD_SHARED_LIBRARY) 
