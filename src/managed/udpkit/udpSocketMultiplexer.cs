9b592e07056242fd5d26fe826edd39683a5ad86f
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-03-16 18:46:12 +0100

    Added UdpSocketMultiplexer.Wait
    UdpSocket.Wait allows you to wait for any socket to have an event.

--- src/managed/udpkit/udpSocketMultiplexer.cs
+++ src/managed/udpkit/udpSocketMultiplexer.cs
@@ -22,10 +22,12 @@
 * THE SOFTWARE.
 */
 
+using System.Threading;
 namespace UdpKit {
     public class UdpSocketMultiplexer {
         int index;
         UdpSocket[] sockets;
+        WaitHandle[] events;
 
         public UdpSocket[] Sockets {
             get { return sockets; }
@@ -34,6 +36,33 @@ namespace UdpKit {
         internal UdpSocketMultiplexer (params UdpSocket[] sockets) {
             this.index = 0;
             this.sockets = sockets;
+            this.events = new AutoResetEvent[this.sockets.Length];
+
+            for (int i = 0; i < this.sockets.Length; ++i) {
+                this.events[i] = this.sockets[i].EventsAvailable;
+            }
+        }
+
+        public bool Wait () {
+            return Wait(-1);
+        }
+
+        public bool Wait (int timeout) {
+            UdpSocket socket;
+            return Wait(timeout, out socket);
+        }
+
+        public bool Wait (int timeout, out UdpSocket socket) {
+            int n = WaitHandle.WaitAny(this.events, timeout);
+
+            if (n >= 0 && n < this.sockets.Length) {
+                index = n;
+                socket = this.sockets[n];
+                return true;
+            }
+
+            socket = null;
+            return false;
         }
 
         public bool Poll (out UdpEvent ev, ref UdpSocket socket) {
