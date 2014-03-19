28f55867f8e790b39a67b6149479396dd0767a66
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-03-19 11:51:37 +0100

    Bumped version to 0.1.6.7

--- src/managed/shared/AssemblyInfo.cs
+++ src/managed/shared/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.6.6")]
-[assembly: AssemblyFileVersion("0.1.6.6")]
+[assembly: AssemblyVersion("0.1.6.7")]
+[assembly: AssemblyFileVersion("0.1.6.7")]
