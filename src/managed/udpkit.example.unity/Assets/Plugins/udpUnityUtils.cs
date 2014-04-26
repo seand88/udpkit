fe5c950aef88167f493420d8dcf70a2d7ffa4b7a
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2014-04-26 06:26:12 +0200

    Updated #if's in udpUnityUtils to properly match all supported platforms

--- src/managed/udpkit.example.unity/Assets/Plugins/udpUnityUtils.cs
+++ src/managed/udpkit.example.unity/Assets/Plugins/udpUnityUtils.cs
@@ -1,19 +1,19 @@
 ﻿using UdpKit;
 
 public static class UdpKitUnityUtils {
-    public static UdpSocket CreatePlatformSpecificSocket<TSerializer> () where TSerializer : UdpSerializer, new() {
-        return CreatePlatformSpecificSocket<TSerializer>(new UdpConfig());
-    }
+  public static UdpSocket CreatePlatformSpecificSocket<TSerializer> () where TSerializer : UdpSerializer, new() {
+    return CreatePlatformSpecificSocket<TSerializer>(new UdpConfig());
+  }
 
-    public static UdpSocket CreatePlatformSpecificSocket<TSerializer> (UdpConfig config) where TSerializer : UdpSerializer, new() {
-#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
-        return UdpSocket.Create<UdpPlatformManaged, TSerializer>(config);
-#elif UNITY_IPHONE
-		return UdpSocket.Create<UdpPlatformIOS, TSerializer>(config);
+  public static UdpSocket CreatePlatformSpecificSocket<TSerializer> (UdpConfig config) where TSerializer : UdpSerializer, new() {
+#if UNITY_IPHONE
+    return UdpSocket.Create<UdpPlatformIOS, TSerializer>(config);
 #elif UNITY_ANDROID
-		return UdpSocket.Create<UdpPlatformAndroid, TSerializer>(config);
+    return UdpSocket.Create<UdpPlatformAndroid, TSerializer>(config);
+#elif UNITY_WEBPLAYER || UNITY_STANDALONE || UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX
+    return UdpSocket.Create<UdpPlatformManaged, TSerializer>(config);
 #else
-		throw new System.NotImplementedException ("UdpKit doesn't support the current platform");
+    throw new System.NotImplementedException("UdpKit doesn't support the current platform");
 #endif
-    }
+  }
 }
